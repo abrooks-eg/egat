70e997cb60bee87aa75535ca4d073eb7f8b3bd44
Matt Lorentz <matt@lorentz.is>
2015-01-29 17:46:50 -0500

    Got the test_runner.py to be installed via PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+#####################################################################################
+#
+# To build the package and upload to PyPi
+#    python setup.py sdist upload 
+#
+# To build and upload to Test PyPi
+#    python setup.py sdist upload -r https://testpypi.python.org/pypi
+#
+#####################################################################################
+
 from distutils.core import setup
 setup(
     name = 'egautotest',
-    packages = ['egautotest'], # this must be the same as the name above
+    packages = ['egautotest', 'egautotest.loggers'], # this must be the same as the name above
     scripts = ['test_runner.py'],
-    version = '0.1.4',
+    version = '0.1.11',
     description = 'An automated testing toolkit',
     author = 'E-gineering LLC',
     author_email = 'eg.pypi@e-gineering.com', # contact email

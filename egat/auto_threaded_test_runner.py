e6b60058374bda7bd1b24361cbbd3f165268a3d8
Matt Lorentz <matt@lorentz.is>
2015-02-23 16:27:03 -0500

    Split the TestRunner into one for auto threads and one for user threads.

---
+++ egat/auto_threaded_test_runner.py
@@ -0,0 +1,177 @@
+from egat.test_workers import WorkManager
+from egat.test_loader import TestLoader
+from egat.test_runner_helpers import WorkProvider
+from egat.test_runner_helpers import WorkerThread
+from threading import Lock
+from egat.testset import ExecutionOrder
+
+class AutoThreadedTestRunner():
+    """A class used to run TestSet tests."""
+    tests = [] # Should be a list of tuples like [(class, [func1, func2]) ...]
+    work_provider = None
+    logger = None
+    number_of_threads = None
+
+    def __init__(self, logger, number_of_threads=1, selenium_debugging=True):
+        """Initializes the AutoThreadedTestRunner. The logger should be a subclass of
+        TestLogger."""
+        self.number_of_threads = number_of_threads
+        self.logger = logger
+        self.work_provider = AutoThreadedWorkProvider()
+
+    def add_tests(self, test_json):
+        """Takes a list of tests in the format required by the configuration file 
+        and adds them to the tests that this TestRunner will run."""
+        tests_objs = AutoThreadedTestRunner._build_tests(test_json)
+        work_nodes = TestLoader.get_work_nodes_for_tests(tests_objs)
+        self.work_provider.add_nodes(*work_nodes)
+
+    def run_tests(self):
+        """Runs the tests that have been added to this AutoThreadedTestRunner."""
+        self.logger.startingTests()
+        workers = []
+
+        for i in range(self.number_of_threads):
+            worker = WorkerThread(self.work_provider, self.logger, thread_num=i)
+            worker.start()
+            workers.append(worker)
+
+        for worker in workers:
+            worker.join()
+        self.logger.finishedTests()
+
+    @staticmethod
+    def _build_tests(test_obj, parent_configuration={}, parent_environment={}):
+        """Takes a JSON test object like the one defined in the configuration file
+        and returns a flat list of test objects in the format that add_tests
+        requires. This method takes care of building out the proper configurations
+        and environments for nested tests."""
+        flat_tests = []
+        # If tests is a test name
+        # Base case
+        if type(test_obj) in [str, unicode]:
+            test = {}
+            test['configuration'] = parent_configuration
+            test['environment'] = parent_environment
+            test['test'] = test_obj
+            flat_tests.append(test)
+
+        # current_tests is a test dict
+        elif type(test_obj) is dict:
+            current_configuration = test_obj.get('configuration', {})
+            current_environments = test_obj.get('environments', {})
+            current_tests = test_obj.get('tests', [])
+
+            # Merge the parent configuration with the current configuration
+            merged_configuration = parent_configuration.copy()
+            for key, val in current_configuration.items():
+                merged_configuration[key] = val
+
+            if current_environments:
+                for current_environment in current_environments:
+                    for current_test in current_tests:
+                        # Merge the parent env with the current env
+                        merged_environment = parent_environment.copy()
+                        for key, val in current_environment.items():
+                            merged_environment[key] = val
+
+                        # Recur
+                        flat_tests += AutoThreadedTestRunner._build_tests(
+                            current_test,
+                            parent_configuration=merged_configuration,
+                            parent_environment=merged_environment
+                        )
+            else:
+                for subtest in current_tests:
+                    flat_tests += AutoThreadedTestRunner._build_tests(
+                        subtest,
+                        parent_configuration=merged_configuration,
+                        parent_environment=parent_environment
+                    )
+
+        return flat_tests
+
+class AutoThreadedWorkProvider(WorkProvider):
+    """A thread-safe class designed to manage WorkNodes and provide them to 
+    WorkerThreads to be executed."""
+    _work_nodes = None
+    _lock = None
+    # Execution groups that have failed. List items can be any type.
+    _failed_ex_groups = None 
+    _cur_resources = None
+
+
+    def __init__(self):
+        self._work_nodes = []
+        self._lock = Lock()
+        self._failed_ex_groups = set()
+        self._cur_resources = set()
+
+    def add_nodes(self, *work_nodes):
+        """Takes a TestSet subclass object and a list of function objects in that 
+        class and adds them as a node in the AutoThreadedWorkProvider's work. The class will be 
+        instantiated and the functions called as tests."""
+        self._lock.acquire()
+        self._work_nodes += work_nodes
+        self._lock.release()
+
+    def get_next_node(self):
+        """Finds and returns the next available node of work and returns it. When 
+        the work is done the 'finished_with_node' method must be called to release 
+        the node's resources. If no work is currently available, this method will 
+        return."""
+        self._lock.acquire()
+
+        for node in self._work_nodes:
+            if AutoThreadedWorkProvider.resources_are_free(node.resources, self._cur_resources):
+                self._cur_resources = self._cur_resources.union(node.resources)
+                self._work_nodes.remove(node)
+                self._lock.release()
+                return node
+
+        self._lock.release()
+
+    def finished_with_node(self, work_node):
+        """Takes a work node and releases its resources. Must be called for each 
+        node returned from 'get_next_node'."""
+        self._lock.acquire()
+        self._cur_resources = self._cur_resources.difference(work_node.resources)
+        self._lock.release()
+
+    def has_work(self):
+        """Returns True if this WorkProvider still has unclaimed work, and False 
+        otherwise."""
+        self._lock.acquire()
+        has_work = self._work_nodes
+        self._lock.release()
+        return has_work
+
+    def add_failed_ex_groups(self, failed_ex_groups):
+        """Takes a list of Execution Groups and adds them to this WorkProvider's list
+        of failed Execution Groups. Should be called if any of this WorkProvider's tests 
+        with execution groups fail."""
+        self._lock.acquire()
+        self._failed_ex_groups = self._failed_ex_groups.union(failed_ex_groups)
+        self._lock.release()
+
+    def has_failed_ex_groups(self, *execution_groups):
+        """Takes a variable number of execution groups and returns True if any of 
+        them have failed and False otherwise."""
+        self._lock.acquire()
+        for ex_group in execution_groups:
+            if ex_group in self._failed_ex_groups:
+                self._lock.release()
+                return True
+
+        self._lock.release()
+        return False
+
+
+    @staticmethod
+    def resources_are_free(required_resources, used_resources):
+        """Takes a list of required resources and returns True if none of them are 
+        in the list of used_resources, and False otherwise."""
+        for required_resource in required_resources:
+            if required_resource in used_resources:
+                return False
+        return True
---
+++ egat/test_runner_helpers.py
@@ -0,0 +1,143 @@
+from collections import deque
+from threading import Thread
+from threading import Lock
+from egat.testset import ExecutionOrder
+import sys
+import os
+import traceback
+
+class WorkProvider():
+    _work_nodes = None
+    _failed_ex_groups = None
+
+    def __init__(self):
+        self._work_nodes = deque()
+        self._failed_ex_groups = set()
+
+    def add_nodes(self, *work_nodes):
+        """Takes a TestSet subclass object and a list of function objects in that 
+        class and adds them as a node in the WorkProvider's work."""
+        self._work_nodes.extend(work_nodes)
+
+    def get_next_node(self):
+        """Finds and returns the next available node of work and returns it. When 
+        the work is done the 'finished_with_node' method must be called. If no work 
+        is currently available, this method will return None."""
+        return self._work_nodes.popleft()
+
+    def finished_with_node(self, work_node):
+        """Takes a work node and releases its resources. Must be called for each 
+        node returned from 'get_next_node'."""
+        pass
+
+    def has_work(self):
+        """Returns True if this WorkProvider still has unclaimed work, and False 
+        otherwise."""
+        return len(self._work_nodes) > 0
+
+    def add_failed_ex_groups(self, failed_ex_groups):
+        """Takes a list of Execution Groups and adds them to this WorkProvider's list
+        of failed Execution Groups. Should be called if any of this WorkProvider's tests 
+        with execution groups fail."""
+        self._failed_ex_groups = self._failed_ex_groups.union(failed_ex_groups)
+
+    def has_failed_ex_groups(self, *execution_groups):
+        """Takes a variable number of execution groups and returns True if any of 
+        them have failed and False otherwise."""
+        for ex_group in execution_groups:
+            if ex_group in self._failed_ex_groups:
+                return True
+        return False
+
+
+
+class WorkerThread(Thread):
+    """This class draws work from the WorkProviderand executes it."""
+    work_provider = None
+    logger = None
+    cur_node = None
+    thread_num = None
+
+    def __init__(self, work_provider, logger, thread_num=None):
+        """Requires a WorkManager, a WorkProvider, and a TestLogger."""
+        super(WorkerThread, self).__init__()
+        self.work_provider = work_provider
+        self.logger = logger
+        self.thread_num = thread_num
+
+    def run(self):
+        """Called when this WorkerThread is started."""
+        while self.work_provider.has_work():
+            cur_node = self.work_provider.get_next_node()
+
+            if cur_node:
+                self.run_tests_for_node(cur_node)
+                self.work_provider.finished_with_node(cur_node)
+
+    def run_tests_for_node(self, node):
+        """Takes a WorkNode and runs the tests it contains."""
+        classname = node.test_class.__name__
+        instance = node.test_class()
+        setattr(instance, 'configuration', node.test_config)
+        setattr(instance, 'environment', node.test_env)
+
+        # if this node contains only one function, check for failed execution groups
+        # and don't instatiate the class or call setup
+        if len(node.test_funcs) == 1:
+            func = node.test_funcs[0]
+            if WorkerThread.has_failed_ex_groups(node.test_class, func, self.work_provider):
+                self.logger.skippingTestFunction(instance, func, thread_num=self.thread_num)
+                return
+                
+        # Try to call the class's setup method
+        if hasattr(instance, 'setup') and callable(instance.setup):
+            instance.setup()
+
+        # Run all the test functions
+        for func in node.test_funcs:
+            # Check for failed execution groups
+            if WorkerThread.has_failed_ex_groups(node.test_class, func, self.work_provider):
+                self.logger.skippingTestFunction(instance, func, thread_num=self.thread_num)
+                continue
+
+            self.logger.runningTestFunction(instance, func, thread_num=self.thread_num)
+            try: 
+                func(instance)
+            except:
+                e = sys.exc_info()[0]
+                tb = traceback.format_exc()
+                self.work_provider.add_failed_ex_groups(
+                    WorkerThread.get_ex_groups(node.test_class, func)
+                )
+
+
+                self.logger.foundException(instance, func, e, tb, thread_num=self.thread_num)
+
+            self.logger.finishedTestFunction(instance, func, thread_num=self.thread_num)
+
+        # Try to call the class's teardown method
+        if hasattr(instance, 'teardown') and callable(instance.teardown):
+            instance.teardown()
+
+    @staticmethod
+    def has_failed_ex_groups(test_class, func, work_provider):
+        """Takes a class and a function object in that class and checks the 
+        WorkProvider to see if any of the Execution Groups the function or class is a
+        member of have failed. Returns True if the function is a member of a failed 
+        Execution Group and False otherwise."""
+        execution_groups = set(
+            WorkerThread.get_ex_groups(test_class, func)
+        )
+        return work_provider.has_failed_ex_groups(*execution_groups)
+
+    @staticmethod
+    def get_ex_groups(*func_or_class):
+        """Takes a variable number of objects and safely tries to get their Execution
+        Groups. Returns either a flat list of all the object's Execution Groups or an 
+        empty list."""
+        if type(func_or_class) is tuple and len(func_or_class) > 1:
+            return [g for ls in map(WorkerThread.get_ex_groups, func_or_class) for g in ls]
+        elif hasattr(func_or_class[0], 'execution_groups'):
+            return func_or_class[0].execution_groups
+        else:
+            return []

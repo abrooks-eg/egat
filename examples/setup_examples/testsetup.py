93f889d940c6936f681ba4350e33e0b8a6ef26b7
Matt Lorentz <matt@lorentz.is>
2015-05-06 16:42:33 -0400

    Updated the setup and teardown example

--- examples/setup_examples/testsetup.py
+++ examples/setup_examples/testsetup.py
@@ -33,7 +33,6 @@ class TestSetupAndTeardown1(SequentialTestSet):
 # will see the browser launch before each test and quit after each test.
 class TestSetupAndTeardown2(UnorderedTestSet):
     def setup(self):
-        assert(False)
         self.browser = webdriver.Firefox()
 
     def testDuck(self):

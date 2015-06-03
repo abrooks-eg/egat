c39a7cae5c57b91ad633fc47c6f191bf255c2843
Matt Lorentz <matt@lorentz.is>
2015-06-03 10:26:08 -0400

    Updated version number to 1.0.0

--- setup.py
+++ setup.py
@@ -21,12 +21,12 @@ setup(
     entry_points = {
         'console_scripts': ['egatest = egat.scripts.egatest:run']
     },
-    version = '0.9.23',
+    version = '1.0.0',
     description = 'An automated testing toolkit',
     author = 'E-gineering LLC',
     author_email = 'eg.pypi@e-gineering.com', # contact email
-    url = 'http://e-gineering.com', # url with information about the package
-    download_url = 'https://bitbucket.org/e-gineering/e-g_automated_testing_poc/get/tag/0.1.tar.gz', # should be a url of the tarball
+    url = 'https://github.com/egineering-llc/egat', # url with information about the package
+    download_url = 'https://github.com/egineering-llc/egat/tarball/1.0.0', # should be a url of the tarball
     keywords = ['testing', 'automated testing', 'functional testing'], # arbitrary keywords
     classifiers = [],
 )

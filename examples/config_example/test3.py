64ba10f3cfc371a91287a1db50248ef95aa5ba1f
abrooks-eg <andrew.brooks@e-gineering.com>
2016-06-30 10:52:19 -0400

    Removing examples for this instance

--- examples/config_example/test3.py
+++
@@ -1,9 +0,0 @@
-from egat.testset import UnorderedTestSet
-
-class Test3(UnorderedTestSet):
-    def testStep1(self):
-        pass
-    def testStep2(self):
-        pass
-    def testStep3(self):
-        pass

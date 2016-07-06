2f869150ff578fe4bdf5a2817fe03e0d4753171c
abrooks-eg <andrew.brooks@e-gineering.com>
2016-07-06 15:40:37 -0400

    Added packages for loggers, scripts

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import setuptools
 from distutils.core import setup
 setup(
     name = 'egat-NCAA',
-    packages = ['egat-NCAA'], # this must be the same as the name above
+    packages = ['egat-NCAA','egat-NCAA.loggers', 'egat-NCAA.scripts'], # this must be the same as the name above
     include_package_data=True,
     package_data = {
         'data': ['default.css', 'egat-header.png']
